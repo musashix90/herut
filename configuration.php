7ae743a331c23ea89d4c5ac1bde0122903ba1363
Navid Adab <digitalxn3@gmail.com>
2008-12-04 22:43:50 -0700

    New fixes, Half way done database + file structure

---
+++ configuration.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+//Site Title
+define(		'SITE_TITLE', 		'Herut Content Management System');
+define(		'SITE_URL', 		'http://localhost/herut');
+
+//Paths
+define(		'BASE_PATH',		__FILE__); //the path to this file is our Base
+
+//Database configuration
+define(		'DB_HOST',			'db.gamecraftx.com');
+define(		'DB_USER',			'herut');
+define(		'DB_PASS',			'95feeb2afb');
+define(		'DB_DB',			'herut');
+define(		'DB_PREFIX',		'her_');
+
+
+//Sessions
+define(		'SESSION_LIFE',		30); //Minutes
+
+
+//Templates
+define(		'DEF_TEM',			'SimplyClean');
+
+
+
+?>
---
+++ includes/core.php
@@ -0,0 +1,16 @@
+
+<?php
+
+//First we get our base information we need
+include("/configuration.php");
+
+//Start session handler
+session_start();
+
+//Capture errors and warnings
+error_reporting(E_ALL);
+
+//Database 
+include("db.php")
+
+?>
