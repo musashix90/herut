621582b7984e98e51ce638402132bfc08989b82e
Navid Adab <digitalxn3@gmail.com>
2008-12-04 17:43:51 -0700

    Index.php changes

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
 //For now I guess we'll leave this here, but we'll put it in a framework once we have a base system done
-if (!file_exists("configuration.php") || is_dir("install/")) {
-	if (file_exists("install/install.php")) {
-	        header("Location: install.php");
+
+//The following test is to check wether the system is already installed. Will be improved in the near future.
+if (!file_exists("configuration.php") && is_dir("install/")) {
+	if (file_exists("install/index.php")) {
+	        header("Location: install/index.php");
 	        die();
 	} else {
 		die("Couldn't find Configuration file nor the installation file");
 	}
 }
+
+
 ?>
