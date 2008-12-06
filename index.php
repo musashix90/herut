a6a402cf8217f120326dbc896c83c2b3e8d59a4d
Michael Tanner <musashix90@gmail.com>
2008-12-05 19:07:41 -0500

    Fixed the difference in the last commit and the fork from digitalxn3

--- index.php
+++ index.php
@@ -4,12 +4,22 @@
 
 //The following test is to check wether the system is already installed. Will be improved in the near future.
 if (!file_exists("configuration.php") && is_dir("install/")) {
-	if (file_exists("install/index.php")) {
-	        header("Location: install/index.php");
-	        die();
-	} else {
-		die("Couldn't find Configuration file nor the installation file");
-	}
+        if (file_exists("install/index.php")) {
+                header("Location: install/index.php");
+                die();
+        } else {
+                die("Couldn't find Configuration file nor the installation file");
+        }
 }
+
+
+//Start session handler
+//Placed here because we want to have a flexible header formation.
+session_start();
+
+
+//This should be on top everything...but needs fixing
+include("includes/core.php");
+
 
 ?>
