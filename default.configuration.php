d3fba88784115ae7b3c817c2b364c8632af21fb0
Michael Tanner <musashix90@gmail.com>
2008-12-06 23:53:48 -0500

    Moved the config file and added an ignore

---
+++ default.configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+//Site Title
+define(		'SITE_TITLE', 		'Herut Content Management System');
+define(		'SITE_URL', 		'http://localhost/herut');
+
+//Paths
+define(		'BASE_PATH',		__FILE__); //the path to this file is our Base
+
+//Database configuration
+define(		'DB_HOST',			'**');
+define(		'DB_USER',			'**');
+define(		'DB_PASS',			'**');
+define(		'DB_DB',			'**');
+define(		'DB_PREFIX',		'her_');
+
+
+//Sessions
+define(		'SESSION_LIFE',		30); //Minutes
+
+
+//Templates
+define(		'DEF_TEM',			'SimplyClean');
+
+?>
