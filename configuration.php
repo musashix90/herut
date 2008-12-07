9c157a3219662198faf107aec1c0ea80544a39c3
Navid Adab <digitalxn3@gmail.com>
2008-12-06 17:05:58 -0700

    Database problem fixed

--- configuration.php
+++ configuration.php
@@ -10,10 +10,10 @@ define(		'SITE_URL', 		'http://localhost/herut');
 define(		'BASE_PATH',		__FILE__); //the path to this file is our Base
 
 //Database configuration
-define(		'DB_HOST',			'db.gamecraftx.com');
-define(		'DB_USER',			'herut');
-define(		'DB_PASS',			'95feeb2afb');
-define(		'DB_DB',			'herut');
+define(		'DB_HOST',			'**');
+define(		'DB_USER',			'**');
+define(		'DB_PASS',			'**');
+define(		'DB_DB',			'**');
 define(		'DB_PREFIX',		'her_');
 
 
