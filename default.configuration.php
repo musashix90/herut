889f7d62e54d4b1d28fb57a2c7913d3ed6a8a180
Michael Tanner <musashix90@gmail.com>
2008-12-17 01:15:05 -0500

    Switched define() function to $config[] array

--- default.configuration.php
+++ default.configuration.php
@@ -1,27 +1,23 @@
 <?php
 
-
-
 //Site Title
-define(		'SITE_TITLE', 		'Herut');
-define(		'SITE_URL', 		'http://localhost/herut');
+$config["SITE_TITLE"] = "Herut";
+$config["SITE_URL"] = "http://localhost/herut";
 
 //Paths
-define(		'BASE_PATH',		dirname(__FILE__)); //the path to this file is our Base
+$config["BASE_PATH"] = dirname(__FILE__); //the path to this file is our Base
 
 //Database configuration
-define(		'DB_HOST',			'**');
-define(		'DB_USER',			'**');
-define(		'DB_PASS',			'**');
-define(		'DB_DB',			'**');
-define(		'DB_PREFIX',		'her_');
-
+$config["DB_HOST"] = "**";
+$config["DB_USER"] = "**";
+$config["DB_PASS"] = "**";
+$config["DB_DB"] = "**";
+$config["DB_PREFIX"] = "her_";
 
 //Sessions
-define(		'SESSION_LIFE',		30); //Minutes
-
+$config["SESSION_LIFE"] = 30; //Minutes
 
 //Templates
-define(		'DEF_TEM',			'SimplyClean');
+$config["DEF_TEM"] = "SimplyClean";
 
 ?>
